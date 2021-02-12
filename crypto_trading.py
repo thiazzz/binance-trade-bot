71766502e912e6ce2ab680c93c9fe465edffc551
Mathijs Gaalman <gaalman@gmail.com>
2021-02-12 10:20:26 +0100

    Added logging to console

--- crypto_trading.py
+++ crypto_trading.py
@@ -23,6 +23,11 @@ fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 logger.info('Started')
 
 # Add supported coin symbols here
